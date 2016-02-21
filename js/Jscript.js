bc1753cce276424c7067dc738b393d304c8c9e9e
Tändja <Tndja>
2016-02-21 19:06:44 +0100

    much changes

---
+++ js/Jscript.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Tändja on 17.02.2016.
+ */
+
+
+$(document).ready(function()
+{
+    console.log("ready!");
+});
+
+// ------------------------------
+// http://twitter.com/mattsince87
+// ------------------------------
+
+function scrollNav() {
+    $('.navbar a').click(function(){
+        //Toggle Class
+        $(".active").removeClass("active");
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.'+theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - 0
+        }, 400);
+        return false;
+    });
+    $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+
